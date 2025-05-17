902aca24abb4b93f6f175b12034c3b3e3796edee
Olly Attwood <oliverattwood@aol.com>
2025-05-17 11:56:05 +0100

    Changed Y-axis range to always be fixed

--- view.py
+++ view.py
@@ -13,6 +13,7 @@ class View:
         self.fig.subplots_adjust(bottom=0.2) #increases bottom space
 
         self.chart = plt.plot(chance_of_profit_list)[0]
+        plt.ylim(top=105, bottom=0) #top is 105 rather than 100 so the graph line is still visible at 100
 
         radio_ax = plt.axes([0.59, 0.21, 0.3, 0.2])
         radio_ax.set_facecolor("#90D5FF")
@@ -33,9 +34,7 @@ class View:
         try:
             new_chance_of_profit_list = self.presenter.get_chance_of_profit_list(float(text))
             self.chart.set_ydata(new_chance_of_profit_list)
-            self.ax.relim() #MAYBE REMOVE THIS LINE AND THE ONE AFTER IF THE X AND Y LABELS ARE CHANGED TO BE FIXED - currently they resize the graph each time
-            self.ax.autoscale_view()
-            self.fig.canvas.draw_idle()
+            self.fig.canvas.draw_idle() #forces the graph to redraw with the new data
         except ValueError:
             root = Tk()
             root.withdraw() #the root has to be created and withrawn otherwise a blank window will appear in the background
